9bf36c38026ed0b8bcf941842c05abbea92ef83a
Sebastián Segura <ssegura30126@ufide.ac.cr>
2024-01-24 18:33:54 -0600

    Creación de proyecto

---
+++ tienda_SebastianSegura/src/main/java/tienda/demo/TiendaSebastianSeguraApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSebastianSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSebastianSeguraApplication.class, args);
+	}
+
+}
